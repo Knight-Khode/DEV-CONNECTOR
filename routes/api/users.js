cc706b1a579645a5be7c61d09ff600b57b3f6fa8
Knight Khode <elmirhamude@gmail.com>
2021-06-03 07:30:59 -0700

    creating users route and hashing password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,51 @@
 const express = require("express")
+const {check,validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+const gravatar = require('gravatar')
 const router = express.Router()
+const User = require('../../models/Users')
 
-router.get('/',(req,res)=>{
-    res.send('Users route')
+router.post('/',[
+    check('name', 'Nmae is required').not().isEmpty(),
+    check('email','Please include a valid email').isEmail(),
+    check('password','Please enter a password with 6 or more characters').isLength({min:6})
+],
+async(req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty())return res.status(400).json({errors:errors.array()})
+    try{
+        const {name,email,password}=req.body
+
+        //see if user exist
+        let user = await User.findOne({email:email})
+        if(user)return res.status(400).json({errors:[{msg:'User Already Exists'}]})
+
+        //find user avatar
+        const avatar = gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        })
+
+        //if not create user
+        user = new User({
+            name,
+            email,
+            password,
+            avatar
+        })
+
+        const salt= await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password,salt)
+        await user.save()
+        res.send('User registered')
+        
+    }catch(ex){
+        console.error(ex.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router
