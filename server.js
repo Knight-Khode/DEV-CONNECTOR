e9a3132518313b46a87636ee4df083d450f2eda2
Knight Khode <elmirhamude@gmail.com>
2021-06-03 04:10:45 -0700

    routes created and tested

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const profile = require('./routes/api/profile')
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const auth = require('./routes/api/auth')
 const app = express()
 
 
 app.use(express.json())
+app.use('/api/profile',profile)
+app.use('/api/users',users)
+app.use('/api/posts',posts)
+app.use('/api/auth',auth)
 
 connectDB()
 const PORT = process.env.PORT || 5000
