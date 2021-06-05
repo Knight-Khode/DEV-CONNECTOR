a7813c474dd89f498c78762174340c2cf96bd567
Knight Khode <elmirhamude@gmail.com>
2021-06-04 20:11:27 -0700

    created route to find current user Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,18 @@
 const express = require("express")
-const router = express.Router()
+const auth = require("../../middleware/auth")
 const Profile = require('../../models/Profile')
+const router = express.Router()
 
 //Get current User Profile
-router.get('/',(req,res)=>{
-    res.send('Profile route')
+router.get('/me',auth,async(req,res)=>{
+    try{
+        const profile = await Profile.findOne({user:req.user.id}).populate('user',['name','avatar'])
+        if(!profile)return res.status(400).send('Could not find user profile')
+        res.json(profile)
+    }catch(err){
+        console.error(error.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 
