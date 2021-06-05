1e147b4c81d49e0e1f31b869d225be47b943e7ea
Knight Khode <elmirhamude@gmail.com>
2021-06-04 23:28:15 -0700

    added education to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -168,4 +168,57 @@ router.put('/experience',[
 
 })
 
+
+//adding user education
+router.put('/education',[
+    auth,
+    check("school","School is required")
+    .not()
+    .isEmpty(),
+    check("degree","Degree is required")
+    .not()
+    .isEmpty(),
+    check("fieldofstudy","Field of study is required")
+    .not()
+    .isEmpty(),
+    check("from","From date is required")
+    .not()
+    .isEmpty()
+],async(req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty())return res.status(400).json({errors:errors.array()})
+    const{
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    }=req.body
+
+    //build exp object
+    const newEdu ={
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    }
+    
+    try{
+        const profile = await Profile.findOne({user:req.user.id})
+        if(!profile)return res.status(400).json({msg:'No profile found'})
+        profile.education.unshift(newEdu)
+        await profile.save()
+        res.json(profile)
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+
+})
+
 module.exports = router
